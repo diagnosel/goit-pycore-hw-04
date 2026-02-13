09c2169cdf41fcd43e8e762d578d64bb769e8a65
AurikaS <aurikas@oddity.com>
2026-02-13 21:47:50 +0200

    added 4 task

--- task_4.py
+++ task_4.py
@@ -1,5 +1,57 @@
+def add_contact(args, contacts):
+    if len(args) != 2:
+        return "Invalid command."
+    
+    name, phone = args
+    
+    contacts[name] = phone
+    return "Contact added."
+
+def change_contact(args, contacts):
+    if len(args) != 2:
+        return "Invalid command."
+    
+    name, phone = args
+    
+    if name in contacts:
+        contacts[name] = phone
+        return "Contact updated."
+    else:
+        return "Contact not found."
+    
+def show_phone(args, contacts):
+    if len(args) != 1:
+        return "Invalid command."
+    
+    name = args[0]
+    
+    if name in contacts:
+        return contacts[name]
+    else:
+        return "Contact not found."
+    
+def show_all(args, contacts):
+
+    if len(args) != 0:
+        return "Invalid command."
+    
+    if not contacts:
+         return "Empty contacts"
+     
+    lines = []
+    for name, phone in contacts.items():
+        lines.append(f"{name}: {phone}")
+
+    return "\n".join(lines)
 
 def parse_input(user_input):
+    parts = user_input.split()
+    if not parts:
+        return "", []
+    else:
+        command = parts[0].lower()
+        args = parts[1:]
+    return command, args
     
 def main():
     contacts = {}
@@ -7,13 +59,26 @@ def main():
 
     while True:
         user_input = input("Enter a command: ")
-        command, *args = parse_input(user_input)
+        command, args = parse_input(user_input)
 
         if command in ["close", "exit"]:
             print("Good bye!")
             break
 
-        # тут буде обробка інших команд
+        elif command == "hello":
+            print("How can I help you?")
 
+        elif command == "add":
+            print(add_contact(args, contacts))
+            
+        elif command == "change":
+            print(change_contact(args, contacts))
+        
+        elif command == "phone":
+            print(show_phone(args, contacts))
+        
+        elif command == "all":
+            print(show_all(args, contacts))
+            
 if __name__ == "__main__":
     main()
